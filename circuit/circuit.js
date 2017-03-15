f6b4127940d8000ef43202c8099e334110e378c9
Sven <mailsvb@gmail.com>
2017-03-15 22:11:04 +0100

    add possibility to update a text item

--- circuit/circuit.js
+++ circuit/circuit.js
@@ -203,6 +203,42 @@ module.exports = (RED) => {
     }
     RED.nodes.registerType("addTextItem",addTextItem);
     
+    //handle outgoing update to text messages to circuit
+    function updateTextItem(n) {
+        RED.nodes.createNode(this,n);
+        let node = this;
+        node.server = RED.nodes.getNode(n.server);
+        
+        node.server.subscribe(node.id, 'state', (state) => {
+            node.status({fill:(state == 'Connected') ? 'green' : 'red',shape:'dot',text:state});
+        });
+        
+        node.on('input', (msg) => {
+            if(node.server.connected) {
+                node.server.client.updateTextItem(msg.payload)
+                .then((item) => {
+                    node.log('item updated');
+                    msg.payload = item;
+                    node.send(msg);
+                })
+                .catch((err) => {
+                    node.error(util.inspect(err, { showHidden: true, depth: null }));
+                    msg.payload = err;
+                    node.send(msg);
+                });
+            }
+            else {
+                node.error('not connected to server');
+            }
+        });
+        
+        node.on('close', () => {
+            node.server.unsubscribe(node.id, 'state');
+            node.send({ payload: {state: 'stopping'} });
+        });
+    }
+    RED.nodes.registerType("updateTextItem",updateTextItem);
+    
     //incoming circuit events
     function Events(n) {
         RED.nodes.createNode(this,n);
